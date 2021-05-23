f59c1a30365cfe04623c0c0ccee6e02e61083082
Mango <evan940415@gmail.com>
2021-05-23 17:43:41 +0800

    path edit

--- bot.py
+++ bot.py
@@ -1,24 +1,27 @@
 import discord
-import json, os
+import json
+import os
 from discord.ext import commands
+from datetime import datetime
 
-with open(r'.\setting\Token.json') as TokenFile:
+with open("./setting/Token.json") as TokenFile:
     TokenData = json.load(TokenFile)
 
-with open(r'.\setting\Setting.json') as SettingFile:
+with open("./setting/botSetting.json") as SettingFile:
     SettingData = json.load(SettingFile)
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=SettingData['Prefix'], intents=intents)
+bot = commands.Bot(command_prefix=SettingData["Prefix"], intents=intents)
 
 @bot.event
 async def on_ready():
-    BotName = SettingData['BotName']
-    print(f'{BotName} is on ready')
+    now = f"[{datetime.now().strftime('%Y/%m/%d %H:%M:%S')}]"
+    BotName = SettingData["BotName"]
+    print(f"{now} >> {BotName} is on ready <<")
 
-for Filename in os.listdir(r'.\commands'):
-    if Filename.endswith('.py'):
-        bot.load_extension(f'commands.{Filename[:-3]}')
+for Filename in os.listdir("./commands"):
+    if Filename.endswith(".py"):
+        bot.load_extension(f"commands.{Filename[:-3]}")
 
 if __name__ == "__main__":
-    bot.run(TokenData['Token'])
+    bot.run(TokenData["Token"])
